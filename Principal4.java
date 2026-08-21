d5a21f1b8b412e78aa8f98bf120f63f55e39004d
dgncesarc <cesarccasanova@hotmail.com>
2026-08-20 21:42:58 -0500

    Se modifico la instancia en la clase principal

--- Principal4.java
+++ Principal4.java
@@ -6,9 +6,9 @@ public class Principal4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MarcoPrincipal4 miMarco=new MarcoPrincipal4();
-		miMarco.setVisible(true);
-		miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		MarcoPrincipal4 miMarcoFinal=new MarcoPrincipal4();
+		miMarcoFinal.setVisible(true);
+		miMarcoFinal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
